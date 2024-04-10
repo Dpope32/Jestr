e0b6b2f2243ca3545e10de230f0bdee64c4fd7cd
Dawson <d.pope@eagles.oc.edu>
2024-04-09 22:19:51 -0500

    Oh so close to having AWS work

--- jestr-web/src/pages/LandingPage.js
+++ jestr-web/src/pages/LandingPage.js
@@ -1,63 +1,152 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './LandingPage.css';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 import { useNavigate } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function LandingPage() {
-  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+  const fileInputRef = useRef(null);
+  const [profilePic, setProfilePic] = useState('');
   const [isLogin, setIsLogin] = useState(false);
+  const [isEmailTaken, setIsEmailTaken] = useState(false);
   const [animationComplete, setAnimationComplete] = useState(false);
+  const [isSignedUp, setIsSignedUp] = useState(false);
+  const [isUsernameAvailable, setIsUsernameAvailable] = useState(false);
+  const defaultProfilePic = "https://via.placeholder.com/150"; 
   const navigate = useNavigate();
 
-  // Hardcoded accounts for testing
-  const accounts = [
-    { username: 'DeeDaw22', password: '1' },
-    { username: 'Poperevo', password: '2' },
-  ];
-
-  const handleSignup = (event) => {
+  const handleSignup = async (event) => {
     event.preventDefault();
-    const accountExists = accounts.some(
-      (account) => account.username === username && account.password === password
-    );
-    if (accountExists) {
-      alert("You already have an account. Would you like to sign in instead?");
-      setIsLogin(true); // Switch to the login form
-    } else {
-      // Proceed with account creation logic
-      console.log('Creating new account with', username, password);
+
+    const userData = {
+      operation: 'signup',
+      email: email,
+      password: password,
+    };
+
+    try {
+      const response = await fetch('https://uxn7b7ubm7.execute-api.us-east-2.amazonaws.com/Test/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userData),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log('Account created:', data);
+        setIsSignedUp(true)
+        toast.success('Account Created Successfully!', {
+          position: "top-center",
+          autoClose: 2000,
+        });
+      } else {
+        throw new Error('Failed to create account');
+      }
+    } catch (error) {
+      console.error('Signup error:', error);
+      alert('An error occurred while signing up. Please try again.');
     }
   };
 
-  const handleLogin = (event) => {
+  const handleCompleteProfile = async () => {
+    const profileData = {
+      operation: 'completeProfile',
+      email: email,
+      username: username,
+      profilePic: profilePic, // This is expected to be a URL
+    };
+  
+    // First, log the original data
+    console.log("Original data being sent:", profileData);
+  
+    const userData = JSON.stringify(profileData);
+  
+    // Then, log the stringified data
+    console.log("Stringified userData:", userData);
+  
+    const requestData = JSON.stringify({ body: userData });
+  
+    // Finally, log the requestData which is the actual body of the fetch request
+    console.log("Request data being sent to the server:", requestData);
+  
+    try {
+      const response = await fetch('https://uxn7b7ubm7.execute-api.us-east-2.amazonaws.com/Test/completeProfile', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: requestData,
+      });
+  
+      // Additional logs can be placed here to log the response
+      if (response.ok) {
+        const data = await response.json();
+        console.log('Profile completed response:', data);
+        navigate('/feed');
+      } else {
+        const errorData = await response.json();
+        console.error('Complete profile error response:', errorData);
+        toast.error(errorData.message || 'Failed to complete profile');
+      }
+    } catch (error) {
+      console.error('Complete profile error caught:', error);
+      toast.error('An error occurred while completing your profile. Please try again.');
+    }
+  };
+  
+  
+  const handleLogin = async (event) => {
     event.preventDefault();
-    const accountExists = accounts.some(
-      (account) => account.username === username && account.password === password
-    );
-    if (accountExists) {
-      // Redirect to Feed.js or change the state of the application to be "logged in"
-      navigate('/feed');
-    } else {
-      alert("Incorrect username or password. Please try again or create an account.");
+
+    const userData = {
+      operation: 'signin',
+      email: email,
+      password: password,
+    };
+
+    try {
+      const response = await fetch('https://uxn7b7ubm7.execute-api.us-east-2.amazonaws.com/Test/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userData),
+      });
+
+      const data = await response.json();
+      if (response.ok && data.signInSuccess) {
+        navigate('/feed');
+      } else {
+        alert(data.message || 'Sign-in failed.');
+      }
+    } catch (error) {
+      console.error('Login error:', error);
+      alert('An error occurred during sign-in. Please try again.');
     }
   };
 
   const toggleForm = () => {
-    setIsLogin(!isLogin); // Toggle the boolean state to switch forms
-    // Reset form fields
-    setUsername('');
+    setIsLogin(!isLogin);
+    setEmail('');
     setPassword('');
+    setUsername('');
+    setProfilePic('');
   };
 
   const formContainerVariants = {
     hidden: {},
     visible: {
       transition: {
-        staggerChildren: 0.2, // Adjust this for the stagger timing between each form element
-        delayChildren: 0.5, // Delay the start of the child animations
-      }
-    }
+        staggerChildren: 0.2,
+        delayChildren: 0.5,
+      },
+    },
   };
 
   const headerVariants = {
@@ -65,38 +154,53 @@ function LandingPage() {
     visible: (custom) => ({
       y: 0,
       opacity: 1,
-      transition: { delay: custom * 0.5 }, // Delay each letter based on its order
-    })
+      transition: { delay: custom * 0.5 },
+    }),
   };
 
-  // Individual item transitions
   const itemVariants = {
     hidden: { y: 20, opacity: 0 },
     visible: {
       y: 0,
       opacity: 1,
       transition: {
-        type: 'linear', // Change to 'linear' for a smoother effect without bounce
-        duration: 1 // Increase duration to slow down the animation
-      }
-    }
-  };
-
-  const handleToggle = () => {
-    setIsLogin(!isLogin);
+        type: 'linear',
+        duration: 1,
+      },
+    },
   };
 
   const handleAnimationComplete = () => {
     setAnimationComplete(true);
   };
 
+  const handleUsernameChange = async (e) => {
+    setUsername(e.target.value);
+    if (e.target.value.length > 0) {
+      // Check if the username is available
+      // You'll need to implement this logic on the server-side
+      setIsUsernameAvailable(true);
+    } else {
+      setIsUsernameAvailable(false);
+    }
+  };
+
+//  const handleProfilePicChange = (e) => {
+//    const file = e.target.files && e.target.files[0];
+//    if (file) {
+//      setProfilePic(URL.createObjectURL(file));
+//    }
+//  };
+
+const handleProfilePicClick = () => {
+  // This sets the default image when the icon is clicked
+  setProfilePic(defaultProfilePic);
+};
+
   return (
     <div className="landing-page-container">
-      <motion.div
-        initial="hidden"
-        animate="visible"
-        className="landing-page"
-      >
+      <ToastContainer position="top-center" autoClose={2000} />
+      <motion.div initial="hidden" animate="visible" className="landing-page">
         <div className="title">
           {['J', 'e', 's', 't', 'r'].map((letter, index) => (
             <motion.span
@@ -114,66 +218,92 @@ function LandingPage() {
         {animationComplete && (
           <motion.div
             className="form-container"
-            variants={formContainerVariants} // Apply container variants here
+            variants={formContainerVariants}
             initial="hidden"
-            animate="visible" // This triggers the child animations
+            animate="visible"
           >
-            <motion.form
-              onSubmit={isLogin ? handleLogin : handleSignup} // Add your form submit handler
-            >
+            <motion.form onSubmit={isLogin ? handleLogin : handleSignup}>
+              <AnimatePresence>
+                {(!isSignedUp || isLogin) && (
+                  <>
+                    <motion.div variants={itemVariants} className="input-container">
+                      <label className="input-label">Email:</label>
+                      <motion.input
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        variants={itemVariants}
+                      />
+                    </motion.div>
+                    <motion.div variants={itemVariants} className="input-container">
+                      <label className="input-label">Password:</label>
+                      <motion.input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        variants={itemVariants}
+                      />
+                    </motion.div>
+                    {!isLogin && ( // This button will be hidden when isLogin is true
+                      <motion.button variants={itemVariants} type="submit" className="signup-btn">
+                        Sign Up
+                      </motion.button>
+                    )}
+                  </>
+                )}
+              </AnimatePresence>
+              {isSignedUp && (
+                <AnimatePresence>
+                  {/* Complete profile section */}
+                  <motion.div variants={itemVariants} className="input-container">
+                  <div className="profile-pic-btn" onClick={handleProfilePicClick}>
+  <span className="plus-icon">+</span>
+</div>
+                    <label className="input-label">Username:</label>
+                    <motion.input
+                      type="text"
+                      value={username}
+                      onChange={handleUsernameChange}
+                      variants={itemVariants}
+                    />
+                    {isUsernameAvailable ? (
+                      <span style={{ color: 'green' }}>Username available!</span>
+                    ) : (
+                      <span style={{ color: 'red' }}>Username unavailable!</span>
+                    )}
+                  </motion.div>
+                  <motion.button
+                    variants={itemVariants}
+                    type="button"
+                    className="complete-profile-btn"
+                    onClick={handleCompleteProfile}
+                  >
+                    Complete Profile
+                  </motion.button>
+                </AnimatePresence>
+              )}
+            </motion.form>
+
+            {!isSignedUp && (
               <motion.div
-                variants={itemVariants} // Use this to animate the label and input as one block
-                className="input-container" // Add this class for styling
-              >
-                <label className="input-label">Username:</label> {/* Add className for styling */}
-                <motion.input
-                  type="text"
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
-                  variants={itemVariants}
-                />
-              </motion.div>
-              <motion.div
-                variants={itemVariants} // Use this to animate the label and input as one block
-                className="input-container" // Add this class for styling
-              >
-               <label className="input-label">Password:</label> {/* Add className for styling */}
-              <motion.input
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
                 variants={itemVariants}
-              />
-            </motion.div>
-            {!isLogin && (
-              <motion.button variants={itemVariants} type="submit">
-                Sign Up
+                onClick={toggleForm}
+                className="toggle-form"
+              >
+                {isLogin ? 'Need an account? Sign up here' : 'Already have an account? Login here'}
+              </motion.div>
+            )}
+
+            {!isSignedUp && !isLogin && (
+              <motion.button variants={itemVariants} className="google-btn">
+                Continue with Google
               </motion.button>
             )}
-            {isLogin && (
-              <motion.button variants={itemVariants} type="submit">
-                Login
-              </motion.button>
-            )}
-           </motion.form>
-            <motion.div
-              variants={itemVariants}
-              onClick={toggleForm}
-              className="toggle-form"
-            >
-              {isLogin ? 'Need an account? Sign up here' : 'Already have an account? Login here'}
-            </motion.div>
-            <motion.button
-              variants={itemVariants}
-              className="google-btn"
-            >
-              Continue with Google
-            </motion.button>
           </motion.div>
         )}
       </motion.div>
     </div>
   );
-}
-
-export default LandingPage;
+  }
+  export default LandingPage;
+  
